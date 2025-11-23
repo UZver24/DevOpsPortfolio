110166650a04985ebb9b867df2e149adb7d9537b
UZver24 <m-kar@inbox.ru>
2025-11-23 11:57:37 +0300

    Контейнеризация

--- backend/run.py
+++ backend/run.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
         "app.main:app",
         host="0.0.0.0",
         port=8000,
-        reload=True
+        reload=False  # Отключаем reload в production (Docker)
     )
 
